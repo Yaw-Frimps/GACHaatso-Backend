1d3b7ac5a23d185d8449e1a1e01c19ee0789b0fe
Yaw Frimpong Yeboah <165104209+YawFrimpong139@users.noreply.github.com>
2026-03-16 14:05:19 +0000

    Initial project setup

---
+++ src/main/java/com/example/gacapp/GacappApplication.java
@@ -0,0 +1,13 @@
+package com.example.gacapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GacappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GacappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gacapp/GacappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gacapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GacappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
